764c48ccc072e0e475cc97d40845928d47ba9f3e
sinister524 <sinister524@gmail.com>
2019-11-11 23:45:48 +0300

    ДЗ № 2

--- task2/src/main/java/DBUtility.java
+++ task2/src/main/java/DBUtility.java
@@ -2,11 +2,15 @@
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class DBUtility {
     /*
     * Каждый из тасков решается одним SQL запросом
      */
+
 
     /*
     Создать таблицу принтеры, Printer(id INTEGER AI PK U, model INTEGER, color TEXT, type TEXT, price INTEGER)
@@ -19,7 +23,14 @@ public class DBUtility {
 
 
 
-    public void createPrinterTable(Connection con, Statement  stmt){
+    public void createPrinterTable(Statement  stmt) throws SQLException {
+        stmt.execute("CREATE TABLE IF NOT EXISTS  Printer (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, model INTEGER, color TEXT, type TEXT, price INTEGER);");
+        stmt.execute("INSERT INTO Printer(model, color, type , price) VALUES (1012, 'col', 'laser', 20000)");
+        stmt.execute("INSERT INTO Product(maker, model, type) VALUES ('HP', 1012, 'Printer')");
+        stmt.execute("INSERT INTO Printer(model, color, type , price) VALUES (1010, 'bw', 'jet', 5000)");
+        stmt.execute("INSERT INTO Product(maker, model, type) VALUES ('Canon', 1010, 'Printer')");
+        stmt.execute("INSERT INTO Printer(model, color, type , price) VALUES (1010, 'bw', 'jet', 5000)");
+        stmt.execute("INSERT INTO Product(maker, model, type) VALUES ('Canon', 1010, 'Printer')");
         //TODO
     }
 
@@ -27,9 +38,22 @@ public class DBUtility {
     * Метод должен вернуть список уникальных моделей PC дороже 15 тысяч
      */
 
-    public ArrayList<String> selectExpensivePC(Statement stmt){
+    public ArrayList<String> selectExpensivePC(Statement stmt) throws SQLException {
+        ArrayList<String> expensivePC = new ArrayList<>();
+        ResultSet expensiveModelPC = stmt.executeQuery("SELECT DISTINCT model FROM PC WHERE price > 15000");
+//        List<Integer> modelsPC = new ArrayList<>();
+        while (expensiveModelPC.next()){
+            expensivePC.add(expensiveModelPC.getString("model"));
+        }
+//        for (int i = 0; i < modelsPC.size(); i++) {
+//            ResultSet rs = stmt.executeQuery("SELECT DISTINCT * from Product WHERE model = " + modelsPC.get(i) + ";");
+//            while (rs.next()){
+//                expensivePC.add(rs.getString(1) + " " + rs.getInt(2)+ " " + rs.getString(3));
+//            }
+//        }
+
         //TODO
-        return null;
+        return expensivePC;
     }
 
     /*
@@ -37,18 +61,40 @@ public class DBUtility {
      * которых выше чем 2500
      */
 
-    public ArrayList<Integer> selectQuickLaptop(Statement stmt){
+    public ArrayList<Integer> selectQuickLaptop(Statement stmt) throws SQLException {
+        ArrayList<Integer> quickLaptop = new ArrayList<>();
+        ResultSet quickModelLaptop = stmt.executeQuery("SELECT DISTINCT id FROM Laptop WHERE speed >= 2500");
+        while (quickModelLaptop.next()){
+            quickLaptop.add(quickModelLaptop.getInt("id"));
+        }
+
         //TODO
-        return null;
+        return quickLaptop;
     }
 
     /*
      * Метод должен вернуть список производителей которые
      *  делают и пк и ноутбуки
      */
-    public ArrayList<String> selectMaker(Statement stmt){
+    public ArrayList<String> selectMaker(Statement stmt) throws SQLException {
+        ArrayList <String> makers = new ArrayList<>();
+        ArrayList <String> makersPC = new ArrayList<>();
+        ArrayList <String> makersLaptop = new ArrayList<>();
+        ResultSet resultSet = stmt.executeQuery("SELECT DISTINCT maker FROM Product WHERE type = 'PC';");
+        while (resultSet.next()){
+            makersPC.add(resultSet.getString("maker"));
+        }
+        resultSet = stmt.executeQuery("SELECT DISTINCT maker FROM Product WHERE type = 'Laptop';");
+        while (resultSet.next()){
+            makersLaptop.add(resultSet.getString("maker"));
+        }
+        for (int i = 0; i < makersPC.size(); i++) {
+            if (makersLaptop.contains(makersPC.get(i))) {
+                makers.add(makersPC.get(i));
+            }
+        }
         //TODO
-        return null;
+        return makers;
     }
 
     /*
@@ -60,8 +106,34 @@ public class DBUtility {
      * или сделать любым другим способом
      */
 
-    public int makerWithMaxProceeds(Statement stmt){
+    public int makerWithMaxProceeds(Statement stmt) throws SQLException {
+        int maxProceeds = Integer.MIN_VALUE;
+        List <String> makers = new ArrayList<>();
+        ResultSet makersSet = stmt.executeQuery("SELECT DISTINCT maker FROM Product;");
+        while (makersSet.next()){
+            makers.add(makersSet.getString("maker"));
+        }
+        for (int i = 0; i < makers.size(); i++) {
+            int currentSum = 0;
+            String maker = makers.get(i);
+            ResultSet makerModelsSet = stmt.executeQuery("SELECT * FROM Product WHERE maker = '" + maker + "';");
+            while (makerModelsSet.next()){
+                String type = makerModelsSet.getString("type");
+                int model = makerModelsSet.getInt("model");
+                System.out.println(maker + " " + model + " " + type);
+//                int modelPrice = 0;
+//                ResultSet modelPricesSet = stmt.executeQuery("SELECT DISTINCT * FROM " + type + " WHERE model = " + model + ";");
+//                while (modelPricesSet.next()){
+//                    modelPrice = modelPricesSet.getInt("price");
+//                }
+//                currentSum = currentSum + modelPrice;
+            }
+//            if (currentSum > maxProceeds) maxProceeds = currentSum;
+            System.out.println();
+        }
         //TODO
-        return 0;
+        return maxProceeds;
     }
+
+
 }
