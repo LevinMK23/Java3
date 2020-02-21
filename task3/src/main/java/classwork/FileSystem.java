7538eef75411e97bff6a4d230e999d9651ebdcef
Mikhail <levinmk23@gmail.com>
2020-02-21 23:14:29 +0300

    classwork 3

--- task3/src/main/java/classwork/FileSystem.java
+++ task3/src/main/java/classwork/FileSystem.java
@@ -14,55 +14,36 @@ import java.util.stream.Stream;
 
 public class FileSystem {
 
-    static String path = "C:\\Users\\Mikhail\\IdeaProjects\\Java31\\task3\\src\\main\\java\\classwork";
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        //System.out.println(Arrays.toString(File.listRoots()));
-        File file = new File(path + "1.txt");
-        File p = new File(path);
-        //p.mkdir();
-        System.out.println(p.isDirectory());
-        //System.out.println(p.getParentFile());
-        //System.out.println(Arrays.toString(p.listFiles()));
-        File parent = p;
-        while (parent != null) {
-            //System.out.println(parent);
-            parent = parent.getParentFile();
-        }
-        dfs(new File("E:\\"), "", 3);
-    }
-
-    static void writeUser(File file, User user) throws IOException {
-        return;
-    }
-
-    static User readUser(File file) throws IOException, ClassNotFoundException {
-        return null;
-    }
-
-    static void writeData(File file, boolean append) throws IOException {
-
-    }
-
-    static void printFileData(File file) throws FileNotFoundException {
-
-    }
-
-    static void createPaths(File file) throws IOException {
-
-    }
-
-    static void dfs(File file, String depth, int d){
-        if (d == 0) return;
+    //if directory -> in if file -> print info
+    //parent -> child
+    static void dfs(String prefix, File file) {
+        if (prefix.length() > 2) return;
         if (file != null) {
-            System.out.println(depth + file);
-            if (file.isDirectory()) {
+            if (file.isFile()) {
+                System.out.println(prefix + file);
+            } else if (file.isDirectory()) {
                 File [] files = file.listFiles();
-                if (files != null) {
-                    for(File f : files) {
-                        dfs(f, depth + "-", d-1);
-                    }
+                if (files == null) return;
+                for (File f : files) {
+                    dfs(prefix + '-', f);
                 }
             }
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        File f = new File("java3_gb.txt");
+        System.out.println(f.getAbsolutePath());
+        f.createNewFile();
+        System.out.println(f.exists());
+        File file = new File("C:\\Users\\Mikhail\\IdeaProjects\\Java31");
+        while (file != null) {
+            System.out.println(file);
+            file = file.getParentFile();
+        }
+        System.out.println(new File("E:\\").getParent());
+        dfs("", new File("E:\\"));
+        //System.out.println(file.exists());
+        //System.out.println(file.isDirectory());
     }
 }
